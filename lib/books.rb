655adf8aeb457ba72432cd48e9bc17af1f8bd5db
Victor Delgado <vdel26@gmail.com>
2014-09-19 01:31:54 +0200

    store past books in redis

--- lib/books.rb
+++ lib/books.rb
@@ -1,5 +1,12 @@
 module Books
   NYT_API_KEY = ENV['NYT_API_KEY']
+
+  def self.redis
+    @@redis ||= begin
+      uri = URI.parse(ENV['REDISTOGO_URL'] || 'redis://localhost:6379')
+      Redis.new(host: uri.host, port: uri.port, password: uri.password)
+    end
+  end
 end
 
 require 'books/server'
--- lib/books/bot.rb
+++ lib/books/bot.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'redis'
 
 module Books
   class Bot
@@ -7,23 +8,33 @@ module Books
       fiction:    'combined-print-and-e-book-fiction',
       nonfiction: 'combined-print-and-e-book-nonfiction',
       advice:     'advice-how-to-and-miscellaneous',
-      business:   'business-books'
     }
 
-    def business_books
-      HTTParty.get("#{NYT_ENDPOINT}#{LISTS[:business]}.json?api-key=#{NYT_API_KEY}")
+    def run
+      books = fiction_books + nonfiction_books + advice_books
+
+      books.each do |book|
+        title = book['book_details'][0]['title']
+        author = book['book_details'][0]['author']
+        url = book['book_details'][0]['amazon_product_url']
+        next unless new?(book)
+        next if tweeted?(title)
+        tweet(title, author, url)
+      end
     end
 
+    private
+
     def fiction_books
-      HTTParty.get("#{NYT_ENDPOINT}#{LISTS[:fiction]}.json?api-key=#{NYT_API_KEY}")
+      HTTParty.get("#{NYT_ENDPOINT}#{LISTS[:fiction]}.json?api-key=#{NYT_API_KEY}")['results']
     end
 
     def nonfiction_books
-      HTTParty.get("#{NYT_ENDPOINT}#{LISTS[:nonfiction]}.json?api-key=#{NYT_API_KEY}")
+      HTTParty.get("#{NYT_ENDPOINT}#{LISTS[:nonfiction]}.json?api-key=#{NYT_API_KEY}")['results']
     end
 
     def advice_books
-      HTTParty.get("#{NYT_ENDPOINT}#{LISTS[:advice]}.json?api-key=#{NYT_API_KEY}")
+      HTTParty.get("#{NYT_ENDPOINT}#{LISTS[:advice]}.json?api-key=#{NYT_API_KEY}")['results']
     end
 
     # check if the book is new in the list
@@ -32,18 +43,14 @@ module Books
     end
 
     # check if book title is in redis
-    def tweeted?(book)
+    def tweeted?(title)
+      Books.redis[title]
     end
 
-    def run
-      b = fiction_books['results']
-      b.each do |book|
-        title = book['book_details'][0]['title']
-        author = book['book_details'][0]['author']
-        next unless new?(book)
-        next if tweeted?(book)
-        puts "#{title} – by #{author}"
-      end
+    def tweet(title, author, url)
+      Books.redis[title] = 'true'
+      puts "#{title} – by #{author}\n\n#{url}"
     end
+
   end
 end
